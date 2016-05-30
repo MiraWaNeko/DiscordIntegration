69d006bf50ee30c799150b82282142fdef63729d
Chikachi <chikachi@chikachi.net>
2016-05-30 16:59:36 +0200

    Disconnect from Discord in one place

--- src/main/java/chikachi/discord/ChikachiDiscord.java
+++ src/main/java/chikachi/discord/ChikachiDiscord.java
@@ -95,6 +95,8 @@ public class ChikachiDiscord {
         if (!receivedStoppingEvent) {
             proxy.onServerCrash();
         }
+
+        DiscordClient.getInstance().disconnect();
     }
 
     @SuppressWarnings("WeakerAccess")
