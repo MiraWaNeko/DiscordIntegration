6c6f2be51c719c74f9e7154646f0071ace7cd451
Chikachi <Chikachi@Chikachi.net>
2016-06-13 21:18:30 +0200

    1.3.0

--- src/main/java/chikachi/discord/Constants.java
+++ src/main/java/chikachi/discord/Constants.java
@@ -20,5 +20,5 @@ package chikachi.discord;
 public class Constants {
     public static final String MODID = "ChikachiDiscord";
     public static final String MODNAME = "Chikachi's Discord integration mod";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 }
