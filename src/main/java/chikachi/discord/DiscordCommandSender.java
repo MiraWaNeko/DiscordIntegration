fa3ec0495fb111e6fb5c3ad0918dc4ea26e2af50
Chikachi <chikachi@chikachi.net>
2018-01-21 20:21:12 +0100

    Combine responses to commands
    Fixes #125
    
    Signed-off-by: Chikachi <chikachi@chikachi.net>

--- src/main/java/chikachi/discord/DiscordCommandSender.java
+++ src/main/java/chikachi/discord/DiscordCommandSender.java
@@ -15,6 +15,7 @@
 package chikachi.discord;
 
 import chikachi.discord.core.Patterns;
+import com.google.common.base.Joiner;
 import com.mojang.authlib.GameProfile;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
@@ -24,13 +25,16 @@ import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.UUID;
+import java.util.*;
 
 @SuppressWarnings("EntityConstructor")
 @ParametersAreNonnullByDefault
 public class DiscordCommandSender extends FakePlayer {
     private static final UUID playerUUID = UUID.fromString("828653ca-0185-43d4-b26d-620a7f016be6");
     private final MessageChannel channel;
+    private List<String> queue;
+    private Timer timer;
+    private TimerTask timerTask;
 
     public DiscordCommandSender(MessageChannel channel, User user) {
         super(FMLCommonHandler.instance().getMinecraftServerInstance().worlds[0], new GameProfile(playerUUID, "@" + user.getName()));
@@ -50,19 +54,42 @@ public class DiscordCommandSender extends FakePlayer {
 
     @Override
     public void sendMessage(ITextComponent component) {
-        this.channel.sendMessage(
-            Patterns.minecraftCodePattern.matcher(
-                component.getUnformattedText()
-            ).replaceAll("")
-        ).queue();
+        this.queueMessage(component);
     }
 
     @Override
     public void sendStatusMessage(ITextComponent component, boolean actionBar) {
-        this.channel.sendMessage(
+        this.queueMessage(component);
+    }
+
+    private void queueMessage(ITextComponent component) {
+        if (queue == null) {
+            queue = new ArrayList<>();
+        }
+        if (this.timer == null) {
+            this.timer = new Timer();
+        }
+        if (this.timerTask != null) {
+            this.timerTask.cancel();
+            this.timer.purge();
+        }
+        this.timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                flushQueue();
+            }
+        };
+        this.timer.schedule(this.timerTask, 500);
+        this.queue.add(
             Patterns.minecraftCodePattern.matcher(
                 component.getUnformattedText()
             ).replaceAll("")
+        );
+    }
+
+    private void flushQueue() {
+        this.channel.sendMessage(
+            Joiner.on("\n").join(this.queue)
         ).queue();
     }
 }
